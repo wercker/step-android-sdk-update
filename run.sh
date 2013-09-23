43997a9e7fdc1a5e5d4a1c564cc36a5116f53ff0
Jacco Flenter <github@putpixel.nl>
2013-09-23 15:58:19 +0200

    check for expect added

--- run.sh
+++ run.sh
@@ -3,6 +3,22 @@ if [ ! -n "$WERCKER_ANDROID_SDK_UPDATE"]; then
     exit 1
 fi
 
+# Install expect if not already available
+if ! type expect &> /dev/null ;
+then
+     info 'expect package not found. installing it'
+     sudo apt-get update
+     sudo apt-get install expect
+
+     if [[ $? -ne 0 ]];then
+         fail 'install of expect failed';
+     else
+         info 'finished installing expect';
+     fi
+else
+    info 'expect is available, and will not be installed by this step'
+fi
+
 ./android-update.exp $WERCKER_ANDROID_SDK_UPDATE
 
 if [ ! -n "$ANDROID_UPDATE_FILTER"];
