c5552151e6cb132e2bab4a917b56a798fc7999c1
Jacco Flenter <github@putpixel.nl>
2013-11-03 09:56:47 +0100

    fixed step

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-echo "switching to: $ANDROID_HOME"
-cd $ANDROID_HOME
-echo pwd
-expect -f $WERCKER_STEP_ROOT/android-update.exp "$WERCKER_ANDROID_SDK_UPDATE_FILTER"
+cd /home/ubuntu/android-sdk-linux
+expect -f $WERCKER_STEP_ROOT/android-update.exp "sysimg-18"
