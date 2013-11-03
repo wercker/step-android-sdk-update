9b4bcd8f48d06389f4b7a84502209c9c29422150
Jacco Flenter <github@putpixel.nl>
2013-11-03 10:14:56 +0100

    stop using $ANDROID_HOME

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-cd "$ANDROID_HOME"
-echo $ANDROID_HOME
-export PACKAGE=sysimg-18
+echo /home/ubuntu/android-sdk-linux
 expect -f $WERCKER_STEP_ROOT/android-update.exp "$WERCKER_ANDROID_SDK_UPDATE_FILTER"
