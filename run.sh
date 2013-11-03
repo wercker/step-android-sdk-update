8f47e73b17791db98f525eb7ae6a3e893a1fa24e
Jacco Flenter <github@putpixel.nl>
2013-11-03 10:20:55 +0100

    use package

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+cd "$ANDROID_HOME"
 echo /home/ubuntu/android-sdk-linux
-expect -f $WERCKER_STEP_ROOT/android-update.exp "$WERCKER_ANDROID_SDK_UPDATE_FILTER"
+export PACKAGE=sysimg-18
+expect -f $WERCKER_STEP_ROOT/android-update.exp "$PACKAGE"
